2fb6b708e0023ee285e756dfa998d547e995129d
MartinNose <liucmartin@qq.com>
2020-04-13 00:28:57 +0800

    bookInsertAPI beginning

--- controllers/bookInsertion.js
+++ controllers/bookInsertion.js
@@ -1,14 +1,14 @@
-let insertBook= (book) => {
-    //insert into book value....
-}
-
+const db = require('./Models');
+let Book = db.book;
 let instBk = (ctx, next) => {
-    //Check get book from ctx
-    //Insert into DB
+    Book.create(ctx.book).then(newbook => console.log(newbook));
 }
 
 let instBks = (ctx, next) => {
-    //
+    books = JSON.parse(ctx.books);
+    for (b in books) {
+        Book.create(b).then(newbook => console.log(newbook));
+    }
 }
 
 module.exports = {
