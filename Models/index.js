3f38949c1c629d89966c1a79dba11705ecd103cb
MartinNose <liucmartin@qq.com>
2020-04-13 00:28:31 +0800

    cancel log

--- Models/index.js
+++ Models/index.js
@@ -27,7 +27,6 @@ sequelize
 fs
   .readdirSync(__dirname)
   .filter(file => {
-      console.log(file);
       return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
