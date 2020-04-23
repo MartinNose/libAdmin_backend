9ab6a5de7ec901a372ae1e9dfcb73dc9f0e11fd3
MartinNose <liucmartin@qq.com>
2020-04-24 02:36:44 +0800

    count api done

--- controllers/count.js
+++ controllers/count.js
@@ -2,9 +2,19 @@ let db = require('../Models');
 let Book = db.book;
 let Borrow = db.borrow;
 let Card = db.card;
+const sequlize = db.sequelize;
 
 let count = async(ctx, next) => {
-    
+    let bookcnt = await Book.count();
+    let cardcnt = await Card.count();
+    let borrowcnt = await Borrow.count();
+
+    ctx.response.status = 200;
+    ctx.response.body = {
+        bkcnt: bookcnt,
+        cdcnt: cardcnt,
+        brcnt: borrowcnt
+    }
 }
 
 module.exports = {
