f1e28facd1389b8c0774167b3fb121bf57f50056
MartinNose <liucmartin@qq.com>
2020-04-26 20:57:57 +0800

    all done

--- controllers/bookSearch.js
+++ controllers/bookSearch.js
@@ -22,7 +22,6 @@ function newquery(con) {
     let where = {};
     validCheck(con);
     for (let i of Query) {
-        console.log(i)
         if (!con.hasOwnProperty(i) || con[i] === '') continue;
         switch(i) {
             case 'start': 
@@ -52,14 +51,12 @@ function newquery(con) {
             default:
                 where[i] = con[i]
         }
-        console.log(where)
     }
     return where;
 }
 
 let search = async (ctx, next) => {
     let where = {}
-    console.log(ctx.request.body)
     try {
         where = newquery(ctx.request.body);
     } catch(e) {
@@ -81,6 +78,28 @@ let search = async (ctx, next) => {
     })
 }
 
+let update = async (ctx, next) => {
+    let newbook = ctx.request.body;
+    let book = await Book.findOne({where:{bno:newbook.bno}})
+    if(!book) {
+        ctx.response.status = 400
+        ctx.response.body = {
+            err: "Illegal book number"
+        }
+        return
+    }
+    for (let i in newbook) {
+        if (book.dataValues.hasOwnProperty(i) && newbook[i]) {
+            book[i] = newbook[i]
+            console.log(book[i])
+        }
+    }
+    await book.save()
+    ctx.response.status = 200
+    ctx.response.body = book
+}
+
 module.exports = {
-    'POST /api/search': search
+    'POST /api/search': search,
+    'POST /api/update': update
 }
